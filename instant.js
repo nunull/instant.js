90a82b2350786c58fcfe1f13b422d990c69e4760
nunull <timmm.albers@gmail.com>
2014-03-13 20:05:29 +0100

    Did some commenting.

--- instant.js
+++ instant.js
@@ -5,7 +5,6 @@
  * Author:  Timm Albers (http://timmalbers.de)
  * Version: 1.0.0
  */
-
 var instant = (function() {
 	var model = {};
 	var views = [];
@@ -18,12 +17,18 @@ var instant = (function() {
 		}
 	};
 
+	/*
+	 * Attaches a property change listener recursivly to the given object.
+	 */
 	var attachPropertyChangeListener = function(o) {
+		// Walk through propertys.
 		for(var property in o) {
 			if(o.hasOwnProperty(property)) {
+				// Recursion.
 				if(typeof o[property] === 'object') {
 					attachPropertyChangeListener(o[property]);
 				} else {
+					// Atach the listener.
 					(function() {
 						var value = o[property];
 						Object.defineProperty(o, property, {
@@ -42,6 +47,9 @@ var instant = (function() {
 		}
 	};
 
+	/*
+	 * Parse the DOM, search for views and initialize those.
+	 */
 	var updateViews = function() {
 		views = [];
 
@@ -53,10 +61,12 @@ var instant = (function() {
 		});
 	};
 
+	/*
+	 * Renders the compiled templates.
+	 */
 	var render = function() {
 		for(var i in views) {
 			var html = config.render(views[i].template, model);
-			// alert(html);
 
 			$(':not(script)[data-view="' + views[i].name + '"]').each(function() {
 				$(this).html(html);
@@ -65,6 +75,9 @@ var instant = (function() {
 	};
 
 	return {
+		/*
+		 * Sets and initializes the model. Can ben used once; or multiple times to overwrite the set model.
+		 */
 		model: function(model_) {
 			attachPropertyChangeListener(model_);
 			model = model_;
@@ -72,10 +85,27 @@ var instant = (function() {
 			updateViews();
 			render();
 
+			// Return instant for method chaning.
 			return instant;
 		},
 
+		/*
+		 * Sets the compiler function.
+		 */
+		compiler: function(compiler) {
+			config.compile = compiler;
+
+			// Return instant for method chaning.
+			return instant;
+		},
+
+		/*
+		 * Sets the renderer function.
+		 */
 		renderer: function(renderer) {
+			config.render = renderer;
+
+			// Return instant for method chaning.
 			return instant;
 		}
 	};
